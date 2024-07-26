a61f50529f7a38fd4e542fdd1857bcdf60907498
Tappok93 <Tappok939393@gmail.com>
2024-07-26 07:45:04 +0300

    Добавление комментариев, как работает Retrofit в коде

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -17,19 +17,27 @@ class FlickrFetchr {
 
     private val flickrApi: FlickrApi
 
+    /**
+     * Создаём экземпляр Retrofit
+     * с базовым URL и
+     * GsonConverterFactory, который используется для преобразования JSON-ответа в объекты Java.
+     */
     init {
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
+        //Cоздается экземпляр интерфейса FlickrApi, который определяет методы для взаимодействия с API Flickr.
+        //Retrofit использует аннотацию GET у FlickrApi интерфейса для генерации кода, который выполняет HTTP-запросы и преобразует ответы в объекты Java.
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
 
     /**
-     * Создаём асинхронный запрос, при успешном запросе вызывается метод onResponse(),
-     * при неудаче метод onFailure(), обновляет значение LiveData, возвращает
-     * результат запроса по API в responseLiveData
+     * Создаём асинхронный запрос .enqueue
+     * В случаи если запрос не прошел - onFailuer и логируем ошибку.
+     * В случаи успешного запроса - onResponce и обрабатываем ответ от сервера и заполняется LiveData с результатом запроса.
+     * Резуьтат работы фун-ции возвращает LiveData с результатом запроса.
      */
     fun fetchPhotos(): LiveData<List<GalleryItem>> {
         val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
